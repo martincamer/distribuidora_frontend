e5be66062077820df2054278922dbcd6b333e529
martincamer <martincamer011@gmail.com>
2024-05-09 09:43:54 -0300

    asd

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { API_URL } from "../config";
 
 const instance = axios.create({
-  baseURL: API_URL,
+  baseURL: "https://distribuidorabackend-production-1769.up.railway.app/api",
   withCredentials: true,
 });
 
